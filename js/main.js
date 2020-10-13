a0ddd58006f022bd37e0cd60fefc7f0ae986322e
sega <49371624+d3942@users.noreply.github.com>
2020-10-13 17:25:08 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Модальное окно
+
+// открыть по кнопке
+$('.js-button-campaign').click(function() { 
+	
+	$('.js-overlay-campaign').fadeIn();
+	$('.js-overlay-campaign').addClass('disabled');
+});
+
+// закрыть на крестик
+$('.js-close-campaign').click(function() { 
+	$('.js-overlay-campaign').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign').fadeOut();
+		
+	}
+});
+
+// открыть по таймеру 
+$(window).on('load', function () { 
+	setTimeout(function(){
+		if($('.js-overlay-campaign').hasClass('disabled')) {
+			return false;
+		} else {
+			
+			$(".js-overlay-campaign").fadeIn();
+		}
+	}, 5000);
+});
+
